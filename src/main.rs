a673105bc69b3b71e4e94b9a6e7f20539ad2e404
keogami <41939011+keogami@users.noreply.github.com>
2022-08-15 00:47:55 +0530

    finalized the arguments for the app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum BusType {
+    System,
+    Session,
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// type of bus to listen on
+    #[clap(short, long, arg_enum)]
+    bus: BusType,
+
+    /// name of the service
+    #[clap(short, long)]
+    name: String,
+
+    /// interface to listen to
+    #[clap(short, long)]
+    interface: String,
+
+    /// path of the object
+    #[clap(short, long)]
+    path: String,
+
+    /// JQL query to filter and manipulate dbus signal body
+    #[clap(short, long)]
+    query: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
