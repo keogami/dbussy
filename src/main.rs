510beeb97467608ae8aca3de78f26d5d362f4461
keogami <41939011+keogami@users.noreply.github.com>
2022-08-18 21:00:59 +0530

    implemented a saner serialization of zvariant values

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, ValueEnum};
 use serde_json::json;
+use zbus::export::serde::Serialize;
+use zbus::export::serde::ser::{SerializeSeq, SerializeTuple};
+use std::collections::HashMap;
 use std::error::Error;
 use zbus::blocking::{Connection, Proxy, SignalIterator};
 use zbus::names::{InterfaceName, BusName};
@@ -54,13 +57,76 @@ where
     Ok(proxy)
 }
 
+struct SaneValue<'a>(zbus::zvariant::Value<'a>);
+
+impl Serialize for SaneValue<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: zbus::export::serde::Serializer
+    {
+        use zbus::zvariant::Value;
+        use std::os::unix::prelude::AsRawFd;
+        use zbus::export::serde::ser::{Error, SerializeMap};
+        match &self.0 {
+            &Value::U8(n) => serializer.serialize_u8(n),
+            &Value::U16(n) => serializer.serialize_u16(n),
+            &Value::U32(n) => serializer.serialize_u32(n),
+            &Value::U64(n) => serializer.serialize_u64(n),
+
+            &Value::I16(n) => serializer.serialize_i16(n),
+            &Value::I32(n) => serializer.serialize_i32(n),
+            &Value::I64(n) => serializer.serialize_i64(n),
+
+            &Value::F64(n) => serializer.serialize_f64(n),
+            &Value::Fd(n) => serializer.serialize_i32(n.as_raw_fd()),
+            &Value::Bool(v) => serializer.serialize_bool(v),
+
+            Value::Str(v) => serializer.serialize_str(v.as_str()),
+            Value::ObjectPath(v) => serializer.serialize_str(v.as_str()),
+            Value::Signature(v) => serializer.serialize_str(v.as_str()),
+            Value::Value(v) => SaneValue((**v).to_owned()).serialize(serializer),
+
+            Value::Array(v) => {
+                let v = v.to_vec();
+                let mut seq = serializer.serialize_seq(Some(v.len()))?;
+                for element in v {
+                    seq.serialize_element(&SaneValue(element))?;
+                }
+                seq.end()
+            },
+
+            Value::Structure(v) => {
+                let v = v.fields();
+                let mut seq = serializer.serialize_tuple(v.len())?;
+                for element in v {
+                    seq.serialize_element(&SaneValue(element.to_owned()))?;
+                }
+                seq.end()
+            },
+
+            Value::Dict(v) => {
+                let d = v.to_owned();
+                let v: HashMap<String, Value> = match HashMap::try_from(d) {
+                    Ok(map) => map,
+                    Err(err) => return Err(Error::custom(format!("Dict couldn't be converted into a HashMap for serialization: {}", err))),
+                };
+                let mut map = serializer.serialize_map(Some(v.len()))?;
+                for (key, value) in v {
+                    map.serialize_entry(&SaneValue(Value::from(key)), &SaneValue(value))?;
+                }
+                map.end()
+            },
+        }
+    }
+}
+
 fn iterate_messages(iter: SignalIterator, jq: &mut jq_rs::JqProgram) -> Result<(), Box<dyn Error>> {
     for message in iter {
         let structure: zbus::zvariant::Structure = message.body()?;
+        let structure = SaneValue(zbus::zvariant::Value::Structure(structure));
 
-        let data = serde_json::to_value(structure.fields())?;
-
-        let signature = serde_json::to_value(structure.full_signature())?;
+        let data = serde_json::to_value(&structure)?;
+        let signature = serde_json::to_value(structure.0.value_signature())?;
 
         let value = json!({
             "data": data,
